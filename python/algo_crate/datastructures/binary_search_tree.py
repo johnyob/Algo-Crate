2be746ccd9d0026d72dfb3dc183fc42830d28cc5
Alistair O'Brien <johnyob132@gmail.com>
2020-02-17 12:54:41 +0000

    Update datastructures. Added BST and linked list

---
+++ python/algo_crate/datastructures/binary_search_tree.py
@@ -0,0 +1,123 @@
+class Node:
+
+    def __init__(self, key, parent=None, left=None, right=None):
+        self.parent = parent
+        self.key = key
+        self.left = left
+        self.right = right
+
+
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = None
+
+    def is_empty(self):
+        return self.root is None
+
+    def transplant(self, u, v):
+        assert u is not None
+
+        if u.parent is None:
+            self.root = v
+        elif u == u.parent.left:
+            u.parent.left = v
+        else:
+            u.parent.right = v
+
+        if v is not None:
+            v.parent = u.parent
+
+    def insert(self, k):
+        if self.is_empty():
+            # ASSERT: the tree is empty
+            self.root = Node(k)
+
+        else:
+            x = self.root
+            y = None
+
+            while x is not None:
+                # ASSERT: x is not a leaf and y is the pointer to x's parent
+                y = x
+                x = x.left if k < x.key else x.right
+
+            # Create the new node z
+            z = Node(k, y)
+
+            # Insert the node
+            if z.key < y.key:
+                y.left = z
+            else:
+                y.right = z
+
+    def delete(self, k):
+        x = self.search(k)
+
+        if x is not None:
+            if x.left is None and x.right is None:
+                self.transplant(x, None)
+            elif x.left is None:
+                self.transplant(x, x.right)
+            elif x.right is None:
+                self.transplant(x, x.left)
+            else:
+                y = self.min(x.right)
+                self.delete(y.key)
+                x.key = y.key
+
+    def search(self, k):
+        x = self.root
+        while x is not None and k != x.key:
+            x = x.left if k < x.key else x.right
+
+        return x
+
+    def min(self, x=None):
+        if x is None:
+            x = self.root
+
+        assert x is not None, "Cannot find the minimum of an empty subtree"
+        while x is not None:
+            x = x.left
+
+        return x
+
+    def max(self, x=None):
+        if x is None:
+            x = self.root
+
+        assert x is not None, "Cannot find the maximum of an empty subtree"
+        while x is not None:
+            x = x.right
+
+        return x
+
+    def successor(self, x=None):
+        if x is None:
+            x = self.root
+
+        assert x is not None, "Cannot find the successor of an empty subtree"
+        if x.right is not None:
+            return self.min(x.right)
+
+        y = x.parent
+        while y is not None and x == y.right:
+            x, y = y, y.parent
+        return y
+
+    def predecessor(self, x=None):
+        if x is None:
+            x = self.root
+
+        assert x is not None, "Cannot find the predecessor of an empty subtree"
+        if x.left is not None:
+            return self.max(x.left)
+
+        y = x.parent
+        while y is not None and x == y.left:
+            x, y = y, y.parent
+        return y
+
+    def traverse(self, traversal_function):
+        return traversal_function(self.root)
--- python/algo_crate/datastructures/linked_list.py
+++ python/algo_crate/datastructures/linked_list.py
@@ -1,9 +1,60 @@
 class Node:
 
-    def __init__(self, v):
-        self.v = v
-        self.next = None
+    def __init__(self, k, next=None, prev=None):
+        self.k = k
+        self.next = next
+        self.prev = prev
 
     def __str__(self):
-        return f"Node: (v: {self.v}, next: {self.next})"
+        return f"Node: (prev: {self.prev}, k: {self.k}, next: {self.next})"
+
+
+class LinkedList:
+
+    def __init__(self):
+
+        # Construct circular sentinel object
+        self.nil = Node(None)
+        self.nil.next = self.nil
+        self.nil.prev = self.nil
+
+        self.len = 0
+
+    def insert(self, k):
+        x = Node(k, self.nil.next)
+
+        self.nil.next.prev = x
+        self.nil.next = x
+        x.prev = self.nil
+
+        self.len += 1
+
+    def search(self, k):
+        x = self.nil.next
+        while x != self.nil and x.k != k:
+            x = x.next
+        return x
+
+    def delete(self, k):
+        assert len(self) > 0, "List underflow"
+        x = self.search(k)
+
+        assert x is not None, f"Node with key {k} not in list L"
+
+        x.prev.next = x.next
+        x.next.prev = x.prev
+
+    def __len__(self):
+        return self.len
+
+    def __str__(self):
+        return f"List: {iter(self)}."
+
+    def __iter__(self):
+        xs = []
+        x = self.nil.next
+        while x != self.nil:
+            xs.append(x.k)
+        return xs
+
 
