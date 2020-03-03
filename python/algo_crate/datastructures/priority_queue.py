aa9edf39225f21b6840f3e5576bd0b9c2aefa41c
johnyob <johnyob132@gmail.com>
2020-03-03 12:24:43 +0000

    Update priority_queue.py. Added decrease key method. Also switched from MaxHeap to MinHeap (original commit contained a BUG)

--- python/algo_crate/datastructures/priority_queue.py
+++ python/algo_crate/datastructures/priority_queue.py
@@ -1,23 +1,35 @@
 from algo_crate.util import DEFAULT_CMP
-from algo_crate.datastructures.heap import MaxHeap
+from algo_crate.datastructures.heap import MinHeap
+
 
 class PriorityQueue:
 
-    def __init__(self, *values):
-        self.q = MaxHeap(*values, cmp=lambda x, y: DEFAULT_CMP(x[0], y[0]))
+    def __init__(self, *values, key=lambda x: x):
+        self.key = key
+        self.q = MinHeap(*values, cmp=lambda x, y: DEFAULT_CMP(key(x), key(y)))
 
     def is_empty(self):
         return len(self) == 0
+
+    def decrease_key(self, x):
+        i = self.q.a.index(x)
+        assert i != -1, f"Item {x} not in queue"
+
+        k = self.key(x)
+        self.q.decrease_key(i, k)
 
     def enqueue(self, x):
         self.q.insert(x)
 
     def dequeue(self):
         assert not self.is_empty(), "Queue underflow"
-        return self.q.extract_max()
+        return self.q.extract_min()
+
+    def __contains__(self, x):
+        return x in self.q
 
     def __len__(self):
         return len(self.q)
 
     def __str__(self):
-        return f"Queue: front -> {self.q} <- tail"
+        return f"PriorityQueue: front -> {self.q} <- tail"
